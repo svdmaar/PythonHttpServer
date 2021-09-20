5747e71a0f0b8b95c852223f9a18de22b70f034a
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-20 10:31:12 -0300

    Adds dynamic form

--- PythonHttpServer.py
+++ PythonHttpServer.py
@@ -5,10 +5,31 @@ port = 8080
 
 class MyHttpHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        responsePrefix ="/response.html?" 
         if self.path == "/":
             # TODO: real "redirect"
             self.path = "/index.html"
             super().do_GET()
+        elif self.path.startswith(responsePrefix):
+            queryString = self.path[len(responsePrefix):]
+            dataFields = queryString.split("&")
+            sum = 0
+            infoStr = ""
+            try:
+                for dataField in dataFields:
+                    numberStr = dataField.split("=")[1]
+                    number = int(numberStr)
+                    sum += number
+                    infoStr += dataField + "<br/>\n"
+            except Exception as error:
+                infoStr = str(error)
+            else:
+                infoStr += "sum: " + str(sum) + "<br/>\n"
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            html = "<html><head><title>Result</title><body><p>" + infoStr + "</p></body></html>"
+            self.wfile.write(bytes(html, "utf8"))
         else:
             super().do_GET()
 
