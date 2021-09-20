a475288d678abe4b91ead8292108793584b964e0
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-20 08:43:26 -0300

    Adds index and basic html pages

--- PythonHttpServer.py
+++ PythonHttpServer.py
@@ -1,1 +1,16 @@
+import http.server
+import socketserver
+
+port = 8080
+
+class MyHttpHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+            super().do_GET()
+        else:
+            super().do_GET()
+
+with socketserver.TCPServer(("", port), MyHttpHandler) as httpd:
+    httpd.serve_forever()
 
